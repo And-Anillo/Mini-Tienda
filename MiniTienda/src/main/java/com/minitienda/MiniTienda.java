21c8f6c6deafad2e25596e2d6b7887e43ea2b224
andersson vargas <anderssoninedjas@gmail.com>
2025-10-08 13:41:09 -0500

    modelo principal de la tienda

--- MiniTienda/src/main/java/com/minitienda/MiniTienda.java
+++ MiniTienda/src/main/java/com/minitienda/MiniTienda.java
@@ -1,6 +1,13 @@
 package com.minitienda;
 
+import javax.swing.*;
+import java.util.*;
+
 public class MiniTienda {
+    static ArrayList<String> nombres = new ArrayList<>();
+    static double[] precios = new double[0];
+    static HashMap<String, Integer> stock = new HashMap<>();
+    static double totalCompras = 0;
     
     public static void main(String[] args) {
         
